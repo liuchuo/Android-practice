914f9eefa51daff368ac7c191a8ca420f1d4055f
liuchuo <95323362@qq.com>
2017-04-26 13:20:09 +0800

    android

---
+++ CustomView/app/src/main/java/net/liuchuo/customview/MainActivity.java
@@ -0,0 +1,19 @@
+package net.liuchuo.customview;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+    }
+}
