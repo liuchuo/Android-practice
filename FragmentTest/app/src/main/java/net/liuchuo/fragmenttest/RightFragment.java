e373480b00e787f7c28d7e9331d46830411709ae
liuchuo <95323362@qq.com>
2017-04-27 23:58:28 +0800

    Fragment

---
+++ FragmentTest/app/src/main/java/net/liuchuo/fragmenttest/RightFragment.java
@@ -0,0 +1,21 @@
+package net.liuchuo.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chenxin on 4/27/17.
+ */
+
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        return view;
+    }
+}
