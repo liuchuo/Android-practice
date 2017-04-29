a5182e1f30388b52d39a11abf51808ef3dbd4329
liuchuo <95323362@qq.com>
2017-04-30 01:16:11 +0800

    布局

---
+++ FragmentNewsApp/app/src/main/java/net/liuchuo/fragmentnewsapp/News.java
@@ -0,0 +1,27 @@
+package net.liuchuo.fragmentnewsapp;
+
+/**
+ * Created by chenxin on 4/30/17.
+ */
+
+public class News {
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
