9f5341150cde1c9fcf89722a7491680fecc7d04a
liuchuo <95323362@qq.com>
2017-05-02 00:16:04 +0800

    Xml

---
+++ FragmentNewsApp/app/src/main/java/net/liuchuo/fragmentnewsapp/NewsContentActivity.java
@@ -0,0 +1,13 @@
+package net.liuchuo.fragmentnewsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+    }
+}
